be860399fec4338591ec26c59afdfe9f9df188c6
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-12-02 23:24:29 +0530

    wallet + harhat

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
-
+import react, { useEffect, useState } from "react";
 export default function Home() {
-  // init wallet
+  const [walletConnected, setWalletConnected] = useState(false);
+
+  const connectWallet = async () => {
+    try {
+      const addresses = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log(addresses);
+      setWalletConnected(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    connectWallet()
+  })
+
 
   return <div className="text-5xl">Wagmi</div>;
 }
