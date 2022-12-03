5146774850928973ca18c5d9fef8d9b5ecd52518
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-12-03 12:48:00 +0530

    thenga

--- Components/Listdisplay.js
+++ Components/Listdisplay.js
@@ -2,48 +2,60 @@ import React from 'react'
 import Playercard from './Playercard';
 import { useState } from 'react';
 
-const Listdisplay = ({players,setPlayers}) => {
+const Listdisplay = ({ players,finalPlayers,setFinalPlayers}) => {
+  const [selectedPlayers,setSelectedPlayers] = useState([])
+  const [currentPlayer,setCurrentPlayer]= useState({name:'',jerseyNo:''})
 
-    function addItem1() {
-        if(selected1 == 11)
-         {
-          console.error("Limit reached");
-          return;
-         }
-        let value = selected1+1;
-        setselected1(value);
-        const player1 = {
-          name: input1,
-          credits: Math.floor(Math.random() * 10),
-        };
-        setplayers1((players1) => [...players1, player1]);
-        
-      }
+  const handleChange = (e)=>{
+    setCurrentPlayer({
+      name : e.target.value,
+      jersyNo : Math.floor(Math.random()*100)
+    })
+    
+  }
 
-      const [input1, setinput1] = useState("");
-      const [players1, setplayers1] = useState([]);
-      const [selected1, setselected1] = useState(0);
+  const addPLayers = (e)=>{
+    if(selectedPlayers.length<11){
+      setSelectedPlayers([...selectedPlayers,currentPlayer])
+      setCurrentPlayer({name:'',jerseyNo:''})
+    }
+    else console.log('limit exceede')
+  }
+
+  const submitHandler = ()=>{
+    var arr = []
+    {selectedPlayers.map((player)=>{
+      arr.push(player.name)
+      setFinalPlayers(current=>[...current,player.name])
+
+    })
+    console.log(arr.length)
+    console.log(arr)
+    players.push(arr)
+  // setFinalPlayers(current =>[...current,arr])
+}
+  }
 
   return (
     <div className="List">
-        <div className=' bg-cyan-300 w-[22rem]'>{selected1}/11</div>
-        <div className="bg-slate-300  text-center w-[22rem] h-[32rem] mr-8 overflow-y-auto">
-            <h4>India</h4>
-            <input
-              type="text"
-              placeholder="Player Name"
-              onChange={(e) => setinput1(e.target.value)}
-            ></input>
-            <button onClick={() => addItem1()}>Add</button>
-            <div className="list-container ">
-              {players1.map((player) => {
-                return (
-                  <Playercard name={player.name} credits={player.credits} />
-                );
-              })}
-            </div>
-            <button>Submit</button>
-          </div>
+      <div className=' bg-cyan-300 w-[22rem]'>{selectedPlayers.length}/11</div>
+      <div className="bg-slate-300  text-center w-[22rem] h-[32rem] mr-8 overflow-y-auto">
+        <h4>India</h4>
+        <input
+          type="text"
+          placeholder="Player Name"
+          onChange={(e)=>handleChange(e)}
+        ></input>
+        <button onClick={addPLayers}>Add</button>
+        <div className="list-container ">
+          {selectedPlayers.map((player) => {
+            return (
+              <Playercard name={player.name} credits={player.credits} />
+            );
+          })}
+        </div>
+        <button onClick={submitHandler}>Submit</button>
+      </div>
     </div>
   )
 }
--- pages/addPlayerstogame/index.js
+++ pages/addPlayerstogame/index.js
@@ -1,18 +1,33 @@
 import React, { useState } from 'react'
 import Listdisplay from '../../Components/Listdisplay';
 function Addplayers() {
-
-  const [players,setPlayers] = useState([])
+const x = [1,2]
+  var players = ["helo"]
+  const [finalPlayers,setFinalPlayers ] = useState([])
   return ( 
-    <div className="h-screen flex flex-col">
+    <div className="">
+      <div className="h-screen flex flex-col">
       <h1 className="text-center my-8 font-bold text-3xl">Admin</h1>
+        {x.map((index)=>{
+     return(
       <div className="bg-slate-500 ">
         <h2 className="text-center mt-8 mb-8">Add Players</h2>
         <div className="flex justify-center h-screen">
-        <Listdisplay setPlayers={setPlayers} players={players}/>
-        <Listdisplay setPlayers={setPlayers} players={players}/>
+
+
+          <Listdisplay players={players} finalPlayers={finalPlayers} setFinalPlayers={setFinalPlayers}/>
         </div>
       </div>
+     )
+        })}
+      
+    </div>
+    {players.length} <br />
+    {finalPlayers.length}
+     {finalPlayers.map((player)=>{
+      return <h3>poda {player}</h3>
+     })}
+
     </div>
   )
 }
