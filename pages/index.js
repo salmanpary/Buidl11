6bed4a84b84f26517fa2ce1ae82d2fb7f0a78c33
adamsyy <adamoommenjacob@gmail.com>
2022-12-03 07:59:57 +0530

    endo cheydu

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import react, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef();
 
   const connectWallet = async () => {
     try {
@@ -15,8 +17,25 @@ export default function Home() {
   }
 
   useEffect(() => {
-    connectWallet()
-  })
+    // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
+    if (!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+       
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+
+      //if metamask extension is not installed, show alert 
+      if (typeof window.ethereum === 'undefined') {
+       alert("Please install MetaMask extension");
+      }
+    
+
+      connectWallet();
+    }
+  }, []);
 
 
   return <div className="text-5xl">Wagmi</div>;
