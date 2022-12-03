e0026d720c1e07c8b580d859324461f7341f6ea4
Amjad Ali <cowboyamjad@gmail.com>
2022-12-03 09:59:21 +0530

    made into components

--- Components/Listdisplay.js
+++ Components/Listdisplay.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import Playercard from './Playercard';
+import { useState } from 'react';
 
 const Listdisplay = (props) => {
+
+    function addItem1() {
+        if(selected1 == 11)
+         {
+          console.error("Limit reached");
+          return;
+         }
+        let value = selected1+1;
+        setselected1(value);
+        const player1 = {
+          name: input1,
+          credits: Math.floor(Math.random() * 10),
+        };
+        setplayers1((players1) => [...players1, player1]);
+      }
+
+      const [input1, setinput1] = useState("");
+      const [players1, setplayers1] = useState([]);
+      const [selected1, setselected1] = useState(0);
+
   return (
-    <div>
-        <div className=' bg-cyan-300 w-[22rem]'>0/11</div>
-        <div className="bg-slate-300  text-center w-[22rem] mr-8">
+    <div className="List">
+        <div className=' bg-cyan-300 w-[22rem]'>{selected1}/11</div>
+        <div className="bg-slate-300  text-center w-[22rem] h-[32rem] mr-8 overflow-y-auto">
             <h4>India</h4>
             <input
               type="text"
               placeholder="Player Name"
-              onChange={(e) => props.setinput1(e.target.value)}
+              onChange={(e) => setinput1(e.target.value)}
             ></input>
-            <button onClick={() => props.addItem1()}>Add</button>
-            <div className="list-container">
-              {props.players1.map((player) => {
+            <button onClick={() => addItem1()}>Add</button>
+            <div className="list-container ">
+              {players1.map((player) => {
                 return (
                   <Playercard name={player.name} credits={player.credits} />
                 );
--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,6 @@ import Playercard from "../Components/Playercard";
 import Listdisplay from "../Components/Listdisplay";
 
 export default function Home() {
-  function addItem1() {
-    if(selected1 == 11)
-     {
-      console.error("Limit reached");
-      return;
-     }
-    let value = selected1+1;
-    setselected1(value);
-    const player1 = {
-      name: input1,
-      credits: Math.floor(Math.random() * 10),
-    };
-    setplayers1((players1) => [...players1, player1]);
-  }
-
-  function addItem2() {
-    let value = selected2+1;
-    setselected2(value);
-    const player2 = {
-      name: input2,
-      credits: Math.floor(Math.random() * 10),
-    };
-    setplayers2((players2) => [...players2, player2]);
-  }
-
-  const [input1, setinput1] = useState("");
-  const [input2, setinput2] = useState("");
-  const [players1, setplayers1] = useState([]);
-  const [players2, setplayers2] = useState([]);
-  const [selected1, setselected1] = useState(0);
-  const [selected2, setselected2] = useState(0);
 
   return (
     <div className="h-screen flex flex-col">
@@ -41,44 +10,8 @@ export default function Home() {
       <div className="bg-slate-500 ">
         <h2 className="text-center mt-8 mb-8">Add Players</h2>
         <div className="flex justify-center h-screen">
-        <div className="List">
-        <div className=' bg-cyan-300 w-[22rem]'>{selected1}/11</div>
-        <div className="bg-slate-300  text-center w-[22rem] h-[32rem] mr-8 overflow-y-auto">
-            <h4>India</h4>
-            <input
-              type="text"
-              placeholder="Player Name"
-              onChange={(e) => setinput1(e.target.value)}
-            ></input>
-            <button onClick={() => addItem1()}>Add</button>
-            <div className="list-container ">
-              {players1.map((player) => {
-                return (
-                  <Playercard name={player.name} credits={player.credits} />
-                );
-              })}
-            </div>
-          </div>
-    </div>
-    <div className="List">
-        <div className=' bg-cyan-300 w-[22rem]'>{selected2}/11</div>
-        <div className="bg-slate-300  text-center w-[22rem] h-[32rem] mr-8 overflow-y-auto">
-            <h4>Pakistan</h4>
-            <input
-              type="text"
-              placeholder="Player Name"
-              onChange={(e) => setinput2(e.target.value)}
-            ></input>
-            <button onClick={() => addItem2()}>Add</button>
-            <div className="list-container">
-              {players2.map((player) => {
-                return (
-                  <Playercard name={player.name} credits={player.credits} />
-                );
-              })}
-            </div>
-          </div>
-    </div>
+        <Listdisplay/>
+        <Listdisplay/>
         </div>
       </div>
     </div>
